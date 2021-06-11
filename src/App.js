7df014748a2cabb26e834bceb9248efa5ae829e7
MahirKh <mkhandaker1999@gmail.com>
2021-06-11 05:13:39 -0400

    Minor Styling Changes & added updated resume

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
@@ -12,32 +12,32 @@ function App() {
   return (
     <>
       <Particles
-      className="particles-canvas"
+        className="particles-canvas"
         params={{
-          particles:{
-            number:{
-              value:30,
-              density:{
-                enable:true,
-                value_area:900
-              }
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
             },
-            shape:{
-              type:"circle",
-              stroke:{
-                width:6,
-                color:"#f9ab00"
-              }
-            }
-          }
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
         }}
-      />  
-      <Navbar/>
-      <Header/>
-      <AboutMe/>
+      />
+      <Navbar />
+      <Header />
+      <AboutMe />
       {/* <Services/> */}
-      <Portfolio/>
-      <Contacts/>
+      <Portfolio />
+      <Contacts />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,44 @@
-import React from 'react'
+import React from "react";
 import Typed from "react-typed";
-import {Link} from "react-scroll"
-import Mahir_Khandaker_Resume from "./MahirKhandakerResume_.pdf"
-
+import { Link } from "react-scroll";
+import Mahir_Khandaker_Resume from "./MahirKhandakerResume_.pdf";
+import linkedin from "../images/linkedin.svg";
 
 const Header = () => {
-    return (
-        <div id="home" className="header-wraper">
-            <div className="main-info">
-                <h3>Hello, </h3>
-                <h1>I'm Mahir</h1>
-                <p>I'm a 4th Year Biomedical and Electrical Engineering Student</p>
-                <Typed
-                    className="typed-text"
-                    strings={["Experienced in: Full-Stack Development", "Experienced in: Nodejs", "Experienced in: React", "Experienced in: HTML/CSS/Javascript","Experienced in: Object-Oriented Programming"]}
-                    typeSpeed={30}
-                    backSpeed={20}
-                    loop
-                />
-                {/* <Link smooth={true} to="contacts" className="btn-main-offer" href="#">contact me</Link> */}
-                <a className="btn-main-offer button" href={Mahir_Khandaker_Resume} download="Mahir_Khandaker_Resume">Download Resume</a>
-
-            </div>
+  return (
+    <div id="home" className="header-wraper">
+      <div className="main-info">
+        <h3>Hello, </h3>
+        <h1>I'm Mahir</h1>
+        <p>I'm a 4th Year Biomedical and Electrical Engineering Student</p>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Experienced in: Full-Stack Development",
+            "Experienced in: Node.js",
+            "Experienced in: React/React Native",
+            "Experienced in: HTML/CSS/Javascript",
+            "Experienced in: Object-Oriented Programming",
+          ]}
+          typeSpeed={30}
+          backSpeed={20}
+          loop
+        />
+        {/* <Link smooth={true} to="contacts" className="btn-main-offer" href="#">contact me</Link> */}
+        <a
+          className="button"
+          href={Mahir_Khandaker_Resume}
+          download="Mahir_Khandaker_Resume"
+        >
+          <p>Download Resume</p>
+        </a>
+        <div className="row">
+          <div>{linkedin}</div>
+          <div>Hello</div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
